3b999aec76e6d1500a18926d36831c0caca201e9
estebanR11 <rubioesteban1107@gmail.com>
2022-06-30 15:27:24 -0500

    Initial commit

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,237 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Board : MonoBehaviour {
+
+	public int width;
+	public int height;
+
+	public int borderSize;
+
+	public GameObject tilePrefab;
+	public GameObject[] gamePiecePrefabs;
+
+	public float swapTime = 0.5f;
+
+	Tile[,] m_allTiles;
+	GamePiece[,] m_allGamePieces;
+
+	Tile m_clickedTile;
+	Tile m_targetTile;
+
+
+	void Start () 
+	{
+		m_allTiles = new Tile[width,height];
+		m_allGamePieces = new GamePiece[width,height];
+
+		SetupTiles();
+		SetupCamera();
+		FillRandom();
+	}
+	
+	void SetupTiles()
+	{
+		for (int i = 0; i < width; i++)
+		{
+			for (int j = 0; j < height; j++)
+			{
+				GameObject tile = Instantiate (tilePrefab, new Vector3(i, j, 0), Quaternion.identity) as GameObject;
+
+				tile.name = "Tile (" + i + "," + j + ")";
+
+				m_allTiles[i,j] = tile.GetComponent<Tile>();
+
+				tile.transform.parent = transform;
+
+				m_allTiles[i,j].Init(i,j,this);
+
+			}
+		}
+	}
+
+	void SetupCamera()
+	{
+		Camera.main.transform.position = new Vector3((float)(width - 1)/2f, (float) (height-1) /2f, -10f);
+
+		float aspectRatio = (float) Screen.width / (float) Screen.height;
+
+		float verticalSize = (float) height / 2f + (float) borderSize;
+
+		float horizontalSize = ((float) width / 2f + (float) borderSize ) / aspectRatio;
+
+		Camera.main.orthographicSize = (verticalSize > horizontalSize) ? verticalSize: horizontalSize;
+
+	}
+		
+	GameObject GetRandomGamePiece()
+	{
+		int randomIdx = Random.Range(0, gamePiecePrefabs.Length);
+
+		if (gamePiecePrefabs[randomIdx] == null)
+		{
+			Debug.LogWarning("BOARD:  " + randomIdx + "does not contain a valid GamePiece prefab!");
+		}
+
+		return gamePiecePrefabs[randomIdx];
+	}
+
+	public void PlaceGamePiece(GamePiece gamePiece, int x, int y)
+	{
+		if (gamePiece == null)
+		{
+			Debug.LogWarning("BOARD:  Invalid GamePiece!");
+			return;
+		}
+
+		gamePiece.transform.position = new Vector3(x, y, 0);
+		gamePiece.transform.rotation = Quaternion.identity;
+
+		if (IsWithinBounds(x,y))
+		{
+			m_allGamePieces[x,y] = gamePiece;
+		}
+
+		gamePiece.SetCoord(x,y);
+	}
+
+	bool IsWithinBounds(int x, int y)
+	{
+		return (x >= 0 && x < width && y>= 0 && y<height);
+	}
+
+	void FillRandom()
+	{
+		for (int i = 0; i < width; i++)
+		{
+			for (int j = 0; j < height; j++)
+			{
+				GameObject randomPiece = Instantiate(GetRandomGamePiece(), Vector3.zero, Quaternion.identity) as GameObject;
+
+				if (randomPiece !=null)
+				{
+					randomPiece.GetComponent<GamePiece>().Init(this);
+					PlaceGamePiece(randomPiece.GetComponent<GamePiece>(), i, j);
+					randomPiece.transform.parent = transform;
+
+				}
+
+
+			}
+		}
+
+	}
+		
+	public void ClickTile(Tile tile)
+	{
+		if (m_clickedTile == null)
+		{
+			m_clickedTile = tile;
+			//Debug.Log("clicked tile: " + tile.name);
+		}
+	}
+
+	public void DragToTile(Tile tile)
+	{
+		if (m_clickedTile !=null && IsNextTo(tile,m_clickedTile))
+		{
+			m_targetTile = tile;
+		}
+	}
+
+	public void ReleaseTile()
+	{
+		if (m_clickedTile !=null && m_targetTile !=null)
+		{
+			SwitchTiles(m_clickedTile, m_targetTile);
+		}
+
+		m_clickedTile = null;
+		m_targetTile = null;
+
+	}
+
+	void SwitchTiles(Tile clickedTile, Tile targetTile)
+	{
+		
+		GamePiece clickedPiece = m_allGamePieces[clickedTile.xIndex,clickedTile.yIndex];
+		GamePiece targetPiece = m_allGamePieces[targetTile.xIndex,targetTile.yIndex];
+
+		clickedPiece.Move(targetTile.xIndex, targetTile.yIndex, swapTime);
+		targetPiece.Move(clickedTile.xIndex, clickedTile.yIndex, swapTime);
+
+
+	}
+
+	bool IsNextTo(Tile start, Tile end)
+	{
+		if (Mathf.Abs(start.xIndex - end.xIndex) == 1 && start.yIndex == end.yIndex)
+		{
+			return true;
+		}
+
+		if (Mathf.Abs(start.yIndex - end.yIndex) == 1 && start.xIndex == end.xIndex)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	List<GamePiece> FindMatches(int startX,int startY,Vector2 searchDir,int minLength = 3)
+    {
+		List<GamePiece> matches = new List<GamePiece>();
+		GamePiece startingPiece = null;
+		if(IsWithinBounds(startX,startY))
+        {
+			startingPiece = m_allGamePieces[startX, startY];
+        }
+
+		if(startingPiece != null)
+        {
+			matches.Add(startingPiece);
+        }
+		else
+        {
+			return null;
+
+        }
+
+		int nextX;
+		int nextY;
+
+		int maxValue = (width > height) ? width : height;
+
+		for(int i =1; i<maxValue-1;i++)
+        {
+			nextX = startX + (int) Mathf.Clamp(searchDir.x,0,1)* i;
+			nextY = startY + (int)Mathf.Clamp(searchDir.y, 0, 1) * i;
+
+			if(!IsWithinBounds(nextX,nextY))
+            {
+				break;
+            }
+			GamePiece nextPiece = m_allGamePieces[nextX, nextY];
+
+			if (nextPiece.animal == startingPiece.animal && nextPiece.color == startingPiece.color && !matches.Contains(nextPiece))
+            {
+				matches.Add(nextPiece);	
+            }
+			else
+            {
+				break;
+            }
+
+		}
+	
+		if(matches.Count >= minLength)
+        {
+			return matches;
+        }
+		return null;
+
+
+    }
+
+}
